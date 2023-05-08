5588bc27db76c9d7d1b0ef31f1fd566cce1a43ec
Ekaterina <ekaterina.2001@live.com>
2023-05-08 18:51:13 +0300

    feat: conversion exception type

---
+++ src/main/java/exceptions/ConversionException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class ConversionException extends Exception {
+    public ConversionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
