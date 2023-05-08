154a19ca8807c5f4fade73c29eae2e86c4c2db29
Ekaterina <ekaterina.2001@live.com>
2023-05-08 18:45:15 +0300

    feat: main database class

---
+++ src/main/java/database/Database.java
@@ -0,0 +1,22 @@
+package database;
+
+import lombok.Getter;
+import lombok.NonNull;
+
+import java.util.Set;
+
+@Getter
+public abstract class Database {
+
+    protected final Set<String> names;
+    protected final String name;
+
+    Database(@NonNull String dbName, Set<String> names) {
+        this.name = dbName;
+        this.names = names;
+    }
+
+    public abstract void lock();
+
+    public abstract void unLock();
+}
